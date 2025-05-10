5c4284a9af85641e68c5f125ed54d9aa3eb816b2
Vid Smole <vid@smole.org>
2025-05-10 22:14:47 +0200

    Premakni HTTP requeste na backend
    Tako nimamo več problemov s CORS

--- web/app.py
+++ web/app.py
@@ -1,12 +1,25 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from bs4 import BeautifulSoup
 import requests
 import sqlite3
 from dataclasses import dataclass
-from flask_cors import CORS
+from googletrans import Translator
+import asyncio
+import re
+import aiohttp
+from typing import Dict, Any
 
 app = Flask(__name__)
-CORS(app)   # Enable CORS for all routes
+loop = asyncio.get_event_loop()
+
+
+REQUEST_TIMEOUT = 1  # Timeout for requests in seconds
+
+@dataclass
+class slovar_result:
+    en: str
+    sl: str
+
 
 @dataclass
 class repozitorij_result:
@@ -20,11 +33,109 @@ class repozitorij_result:
     stevilka_strani_pdf: list[int]
 
 
-@app.post("/slovar/repozitorij")
-def slovar_repozitorij():
-    query = request.json["query"]
-    page = request.json["page"]
+def dis_slovarcek(query: str) -> list[slovar_result]:
+    print("DIS Slovarček: ", query)
 
+    response = requests.get(f"https://dis-slovarcek.ijs.si/search?search_query={query}", timeout=REQUEST_TIMEOUT)
+
+    if response.status_code != 200:
+        print(
+            "Error accessing https://dis-slovarcek.ijs.si. Status code:",
+            response.status_code,
+        )
+        return
+
+    soup = BeautifulSoup(response.content, "html.parser")
+    result_containers = soup.find_all(id="all-search-results")
+
+    results = []
+    for result_container in result_containers:
+        result = result_container.find(class_="accordion")
+
+        en = result.find(class_="search-result-left").text.strip()
+        sl = result.find(class_="search-result-right").text.strip()
+
+        results.append(slovar_result(en, sl))
+
+    return results
+
+
+def ltft(query: str) -> list[slovar_result]:
+    print("LTFE: ", query)
+    base_url = "http://slovar.ltfe.org/index/add"
+    results = []
+
+    def parse_results(full_url, source_lang):
+        try:
+            response = requests.get(full_url, timeout=REQUEST_TIMEOUT)
+            response.raise_for_status()
+        except requests.RequestException as e:
+            print(f"Error accessing {full_url}: {e}")
+            return
+
+        soup = BeautifulSoup(response.text, "html.parser")
+        result_containers = soup.select(".wHead")
+
+        for container in result_containers:
+            try:
+                if source_lang == "eng":
+                    sl = container.select_one(".lang").get_text(strip=True)
+                    en = container.contents[0].strip()
+                else:
+                    en = container.select_one(".lang").get_text(strip=True)
+                    sl = container.contents[0].strip()
+
+                results.append(slovar_result(en, sl))
+            except Exception as e:
+                print(f"Error parsing entry: {e}")
+
+    parse_results(f"{base_url}/eng/?q={query}&type=all", source_lang="eng")
+    parse_results(f"{base_url}/slo/?q={query}&type=all", source_lang="slo")
+
+    return results
+
+
+def ijs(query: str) -> list[slovar_result]:
+    print("IJS: ", query)
+    url = f"https://www.ijs.si/cgi-bin/rac-slovar?w={query}"
+    response = requests.get(url, timeout=REQUEST_TIMEOUT)
+
+    if not response.ok:
+        print(f"Error accessing {url}. Status code: {response.status_code}")
+        return []
+
+    # Na IJS ne znajo generirati pravilnega HTML-ja, zato ne moremo le uporabiti parserja od BeautifulSoup
+    # Na srečo lahko parsamo HTML z regexom https://stackoverflow.com/a/1732454
+    pattern = r"<dt>([^<]+)<dd>([^<]+)"
+    pairs = re.findall(pattern, response.text)
+
+    results = []
+    for pair in pairs:
+        en = pair[0].strip()
+        sl = pair[1].strip()
+
+        # Popravi šumnike
+        sl = sl.replace('"c', "č")
+        sl = sl.replace('"C', "Č")
+        sl = sl.replace('"s', "š")
+        sl = sl.replace('"S', "Š")
+        sl = sl.replace('"z', "ž")
+        sl = sl.replace('"Z', "Ž")
+
+        results.append(slovar_result(en, sl))
+
+    return results
+
+
+def google_translate(query: str) -> list[slovar_result]:
+    print("Google Translate: ", query)
+    translator = Translator()
+    result = loop.run_until_complete(translator.translate(query, dest="sl"))
+    return [slovar_result(query, result.text)]
+
+
+def repozitorij(query: str, page: int) -> list[repozitorij_result]:
+    print("Repozitorij: ", query, "page:", page)
     page_size = 50
 
     conn = sqlite3.connect("slovar.db")
@@ -42,7 +153,9 @@ def slovar_repozitorij():
         cursor.execute(avtorji_query, (stran[0],))
         avtorji = cursor.fetchall()
 
-        organizacije_query = """SELECT ime_kratko FROM organizacije JOIN gradiva_organizacije ON organizacije.id = gradiva_organizacije.organizacija_id WHERE gradivo_id = ?"""
+        organizacije_query = (
+            """SELECT ime_kratko FROM organizacije JOIN gradiva_organizacije ON organizacije.id = gradiva_organizacije.organizacija_id WHERE gradivo_id = ?"""
+        )
         cursor.execute(organizacije_query, (stran[0],))
         organizacije = cursor.fetchall()
 
@@ -64,31 +177,70 @@ def slovar_repozitorij():
     return results
 
 
-@app.post("/slovar/dis")
-def slovar_dis():
-    query = request.json["query"]
+async def najdi_rezultate(query: str, repozitorij_page: int, enabled_slovarji: Dict[str, bool]) -> Dict[str, Any]:
+    loop = asyncio.get_event_loop()
+    results = {}
 
-    response = requests.get(f"https://dis-slovarcek.ijs.si/search?search_query={query}")
+    tasks = {}
 
-    # Check if the request was successful
-    if response.status_code != 200:
-        print(
-            "Error accessing https://dis-slovarcek.ijs.si. Status code:",
-            response.status_code,
-        )
-        return
+    if enabled_slovarji.get("dis_slovarcek"):
+        tasks["dis_slovarcek"] = loop.run_in_executor(None, dis_slovarcek, query)
 
-    soup = BeautifulSoup(response.content, "html.parser")
-    result_containers = soup.find_all(id="all-search-results")
+    if enabled_slovarji.get("ltfe"):
+        tasks["ltfe"] = loop.run_in_executor(None, ltft, query)
 
-    results = []
+    if enabled_slovarji.get("ijs"):
+        tasks["ijs"] = loop.run_in_executor(None, ijs, query)
 
-    for result_container in result_containers:
-        result = result_container.find(class_="accordion")
+    if enabled_slovarji.get("google_translate"):
+        tasks["google_translate"] = loop.run_in_executor(None, google_translate, query)
 
-        en = result.find(class_="search-result-left").text.strip()
-        sl = result.find(class_="search-result-right").text.strip()
+    if enabled_slovarji.get("repozitorij"):
+        tasks["repozitorij"] = loop.run_in_executor(None, repozitorij, query, repozitorij_page)
 
-        results.append({"en": en, "sl": sl})
+    completed = await asyncio.gather(*tasks.values(), return_exceptions=True)
+
+    for key, value in zip(tasks.keys(), completed):
+        results[key] = value
 
     return results
+
+
+@app.route("/")
+def index():
+
+    # Privzete vrednosti za checkboxe
+    enabled_slovarji = {"dis_slovarcek": True, "ltfe": False, "ijs": True, "google_translate": True, "repozitorij": False}
+
+    return render_template("index.html", enabled_slovarji=enabled_slovarji)
+
+
+@app.route("/search")
+def search():
+
+    query = request.args.get("query", "", type=str)
+    repozitorij_page = request.args.get("repozitorij-page", 1, type=int)  # Za repozitorij
+
+    # Ker uporabljamo navaden HTML form bodo checkboxi, ki niso checked izpuščeni iz requesta
+    enabled_slovarji = {
+        "dis_slovarcek": "dis-slovarcek" in request.args and request.args["dis-slovarcek"] == "on",
+        "ltfe": "ltfe" in request.args and request.args["ltfe"] == "on",
+        "ijs": "ijs" in request.args and request.args["ijs"] == "on",
+        "google_translate": "google-translate" in request.args and request.args["google-translate"] == "on",
+        "repozitorij": "repozitorij" in request.args and request.args["repozitorij"] == "on",
+    }
+
+    # Requeste na vse slovarje izvedemo hkrati, da prihranimo čas
+    results = asyncio.run(najdi_rezultate(query, repozitorij_page, enabled_slovarji))
+
+    return render_template(
+        "search.html",
+        query=query,
+        repozitorij_page=repozitorij_page,
+        enabled_slovarji=enabled_slovarji,
+        results=results,
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
